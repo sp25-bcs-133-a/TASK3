ad60a5a9604e46beb0c48546325e46a0cc54d2a8
sp25-bcs-133-a <sp25-bcs-133@cuilahore.edu.pk>
2025-09-10 17:11:49 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main{
 	public static void main(String[] args){
 		Employee e1 = new Employee("Tayyaba", 102789, 20000, "COMSATS University Islamabad");
+		Employee e2 = new Employee("Adila", 163736, 45600, "UAE");
 
 		e1.display();
+		e2.display();
 		System.out.println("Updating Name...");
 		e1.setName("Tayyaba Riaz");
 		System.out.println(e1.getName());
